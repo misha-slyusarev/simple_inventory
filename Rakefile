b106d5531d3c39628ca2500272c506693fddf3fc
Misha Slyusarev <mik3weider@gmail.com>
2015-04-27 17:06:04 -0300

    Begin to work on the install generator.

--- Rakefile
+++ Rakefile
@@ -8,16 +8,11 @@ require 'rdoc/task'
 
 RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'
-  rdoc.title    = 'SimpleInventoryManagement'
+  rdoc.title    = 'SimpleInventory'
   rdoc.options << '--line-numbers'
   rdoc.rdoc_files.include('README.md')
   rdoc.rdoc_files.include('lib/**/*.rb')
 end
-
-
-
-
-
 
 Bundler::GemHelper.install_tasks
 
--- lib/generators/install_generator.rb
+++ lib/generators/install_generator.rb
@@ -1,7 +1,19 @@
 module SimpleInventory
   class InstallGenerator < Rails::Generators::Base
+    argument :table_prefix, type: :string, default: 'SimpleInventory'
+
     def install
-      generate 'migration', 'CreateSimpleInventory model:string instance_id:integer amount:integer'
+      table_prefix_camelized = table_prefix.camelize
+
+      create_file "config/initializers/#{table_prefix_camelized}.rb" do
+      end
+
+      generate 'migration', "Create#{table_prefix_camelized}Model name:string default_amount:integer"
+      generate 'migration', "Create#{table_prefix_camelized}Instance model_id:integer amount:integer"
+      generate 'migration', "Create#{table_prefix_camelized}Freeze model_id:integer instance_id:integer amount:integer"
+      generate 'migration', "Create#{table_prefix_camelized}Log model_id:instance instance_id:integer comment:integer"
+
+      table_prefix_snaked = table_prefix.underscore
       rake 'db:migrate'
     end
   end
